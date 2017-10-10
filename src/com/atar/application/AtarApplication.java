41859813878f9f8f1b4c8c37c598bc8845014950
王贵 <283495283@qq.com>
2017-10-10 17:45:11 +0800

    gfg

--- src/com/atar/application/AtarApplication.java
+++ src/com/atar/application/AtarApplication.java
@@ -56,8 +56,8 @@ public class AtarApplication extends Application {
 
 		CommonApplication.initApplication(this);// 初始化全局Context
 		CommonNetWorkExceptionToast.initToastError(this, R.array.err_toast_string);// 初始化全局网络错误提示信息
-		ShowLog.setDebug(true);// 设置不显示日志 上线前记得改成false
-		CommonNetWorkExceptionToast.setIsShowErrorToast(true);// 上线前记得设置不显示错误网络具体提示 测试时可开启
+		ShowLog.setDebug(false);// 设置不显示日志 上线前记得改成false
+		CommonNetWorkExceptionToast.setIsShowErrorToast(false);// 上线前记得设置不显示错误网络具体提示 测试时可开启
 		CommonApplication.initImageLoader(getApplicationContext());// 初始化加载图片配置
 		// CommonToast.initToastResouseId(R.drawable.corners_toast, R.color.black);// 初始化toast字体颜色和背景
 		CrashHandler.getInstance().init(this);// 接收错误异常
--- src/com/atar/weex/utils/WeexUtils.java
+++ src/com/atar/weex/utils/WeexUtils.java
@@ -41,16 +41,16 @@ import com.taobao.weex.ui.SimpleComponentHolder;
  *****************************************************************************************************************************************************************************
  */
 public class WeexUtils {
-	// /**ip*/
-	public static final String IP = AppConfigModel.getInstance().getString("WEEX_IP_KEY", "192.168.1.10:8080");
-	// /**网络请求方式http or https*/
-	public static final String HTTP = IP.contains("com.cn") ? "https://" : "http://";
+	// // /**ip*/
+	// public static final String IP = AppConfigModel.getInstance().getString("WEEX_IP_KEY", "192.168.1.10:8080");
+	// // /**网络请求方式http or https*/
+	// public static final String HTTP = IP.contains("com.cn") ? "https" : "http";
 
 	/**打包上线时用 注意 注释掉上面2行 放开下面2行 不用走这么多判断*/
-	// public static final String IP = "js.taoguba.com.cn/weex";
-	// public static final String HTTP = "https";
+	public static final String IP = "raw.githubusercontent.com/wgllss/WeexDemo/master";
+	public static final String HTTP = "https";
 	/**weex 服务器地址*/
-	public static final String WEEX_HOST = HTTP + IP + "/";
+	public static final String WEEX_HOST = HTTP + "://" + IP + "/";
 
 	public static final String HTML_TEST_URL = WEEX_HOST + "assets/html/index.html";
 
